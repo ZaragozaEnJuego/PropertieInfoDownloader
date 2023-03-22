73f42a9d3fbc4a70bde79011e722f94a958cd8c4
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-03-22 16:04:32 +0100

    more kinds

--- main.go
+++ main.go
@@ -7,48 +7,52 @@ import (
 )
 
 type Node struct {
-	ID     int64    `json:"id"`
-	Lat    float64  `json:"lat"`
-	Lon    float64  `json:"lon"`
-	Tags   map[string]string `json:"tags"`
+	ID   int64             `json:"id"`
+	Lat  float64           `json:"lat"`
+	Lon  float64           `json:"lon"`
+	Tags map[string]string `json:"tags"`
 }
 
 type Response struct {
-	Version     float64 `json:"version"`
-	Generator   string  `json:"generator"`
-	Nodes       []Node  `json:"elements"`
+	Version   float64 `json:"version"`
+	Generator string  `json:"generator"`
+	Nodes     []Node  `json:"elements"`
 }
 
 func main() {
 	//selecionar los tipos de edificios
-    amenities:=[]string{"restaurant","bar","school"}
+	amenities := []string{"restaurant", "bar", "school", "bus_station", "taxi", "library", "school", "university", "college", "clinic", "hospital", "pharmacy", "cafe", "fast_food"}
 
-    //generar la url
-    url:="https://overpass-api.de/api/interpreter?data=[out:json];area[name=\"Zaragoza\"]->.a;"
-    for _, v := range amenities {
-        url+=fmt.Sprintf("node[\"amenity\"=\"%s\"][\"name\"](area.a);",v)
-        
-    }
-    url+="out;"
-    // Realizar la petición GET a la API de Overpass Turbo
-    resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error al realizar la petición:", err)
-		return
-	}
-	defer resp.Body.Close()
+	var edificios []Node
 
-	// Leer la respuesta de la API y decodificarla en un struct Response
-	var data Response
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-        fmt.Println(resp.Body)
-		fmt.Println("Error al decodificar la respuesta:", err)
-		return
+	for _, am := range amenities {
+		//generar la url
+
+		url := fmt.Sprintf("https://overpass-api.de/api/interpreter?data=[out:json];area[name=\"Zaragoza\"]->.a;node[\"amenity\"=\"%s\"][\"name\"](area.a);out;", am)
+
+		// Realizar la petición GET a la API de Overpass Turbo
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println("Error al realizar la petición:", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		// Leer la respuesta de la API y decodificarla en un struct Response
+		var data Response
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		if err != nil {
+			fmt.Println(resp.Body)
+			fmt.Println("Error al decodificar la respuesta:", err)
+			return
+		}
+		edificios = append(edificios, data.Nodes...)
+
 	}
 
 	// Imprimir los resultados
-	for _, node := range data.Nodes {
+
+	for _, node := range edificios {
 		fmt.Println("ID:", node.ID)
 		fmt.Println("Latitud:", node.Lat)
 		fmt.Println("Longitud:", node.Lon)
@@ -56,4 +60,5 @@ func main() {
 		fmt.Println("Tipo:", node.Tags["amenity"])
 		fmt.Println()
 	}
+
 }
